8a1a022b7cf108726233c14dcbfc00fac1f75402
cperciva <cperciva@4dbf38cc-6022-0410-aae9-1d1eeb228fa7>
2012-08-18 10:05:27 +0000

    Source tree from which scrypt-1.1.1 release was built.

--- trunk/scrypt-nosse.c
+++ trunk/scrypt-nosse.c
@@ -293,9 +293,9 @@ scrypt(const uint8_t * passwd, size_t passwdlen,
 #ifdef MAP_ANON
 	if ((V0 = mmap(NULL, 128 * r * N, PROT_READ | PROT_WRITE,
 #ifdef MAP_NOCORE
-	    MAP_ANON | MAP_PRIVATE,
-#else
 	    MAP_ANON | MAP_PRIVATE | MAP_NOCORE,
+#else
+	    MAP_ANON | MAP_PRIVATE,
 #endif
 	    -1, 0)) == MAP_FAILED)
 		goto err2;
