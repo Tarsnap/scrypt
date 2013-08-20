35d7d18d9faa86a75888816a6bc83a117a96fde1
cperciva <cperciva@4dbf38cc-6022-0410-aae9-1d1eeb228fa7>
2013-08-20 00:13:07 +0000

    Clean up build gunk.

--- trunk/config_freebsd.h
+++ trunk/config_freebsd.h
@@ -1,8 +1,5 @@
 /* A default configuration for FreeBSD, used if there is no config.h. */
 
-#define HAVE_ERR_H 1
-#define	HAVE_SYS_ENDIAN_H 1
-#define HAVE_DECL_BE64ENC 1
 #define HAVE_POSIX_MEMALIGN 1
 #define HAVE_SYSCTL_HW_USERMEM 1
 #define HAVE_CLOCK_GETTIME 1
