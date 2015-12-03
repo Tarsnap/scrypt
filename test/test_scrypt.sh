3af20000439c9d6d4aaae853250150742258c345
zhroom <40bytectf@gmail.com>
2015-12-03 05:45:56 -0600

    Fixed typo in configure.ac and removed bash refs
    from the test_scrypt.sh

--- test/test_scrypt.sh
+++ test/test_scrypt.sh
@@ -2,7 +2,6 @@
 
 
 # make sure we have the files and are runnning inside the test directory
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PDIR=$(pwd)
 
 f1="test_scrypt"
@@ -10,39 +9,27 @@ f2="test_scrypt.good"
 f3="test_scrypt.sh"
 
 EXISTS=false
-TDIR=false
 
-if [ ${DIR} == ${PDIR}"/test" ];
+if [ -e ${PDIR}/${f1} ] && [ -e ${PDIR}/${f2} ] && [ -e ${PDIR}/${f3} ];
 then
-    TDIR=true
-    if [ -e ${DIR}/${f1} ] && [ -e ${DIR}/${f2} ] && [ -e ${DIR}/${f3} ];
-    then
-        EXISTS=true
-    fi
-fi
-
-
-if [ ${TDIR} == false ];
-then
-    echo "test_scrypt.sh must be run from the parent directory.  e.g. test/test_scrypt.sh"
-    exit 1
+    EXISTS=true
 fi
 
 if [ ${EXISTS} == false ];
 then
     echo "One or more of the following files is missing:"
-    echo "  ${DIR}/${f1}"
-    echo "  ${DIR}/${f2}"
-    echo "  ${DIR}/${f3}"
+    echo "  ${PDIR}/${f1}"
+    echo "  ${PDIR}/${f2}"
+    echo "  ${PDIR}/${f3}"
     echo "Please correct and rerun test_scrypt.sh"
     exit 2
 fi
 
 # if we got here, run the tests
 
-${DIR}/test_scrypt > ${DIR}/test_scrypt.log
-if cmp -s ${DIR}/test_scrypt.log ${DIR}/test_scrypt.good; then
-	rm ${DIR}/test_scrypt.log
+./test_scrypt > ./test_scrypt.log
+if cmp -s ./test_scrypt.log ./test_scrypt.good; then
+	rm ./test_scrypt.log
 	echo "TESTS PASSED!"
 	exit 0
 else
