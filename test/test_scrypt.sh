229980f78c43e06d5b12ff8f528026b4ff47ce77
Graham Percival <gperciva@tarsnap.com>
2016-03-09 16:45:43 -0800

    build: add "make test"
    This is functionality split off from @bigendiansmalls' PR:
    https://github.com/Tarsnap/scrypt/pull/22

--- test/test_scrypt.sh
+++ test/test_scrypt.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-./test_scrypt > test_scrypt.log
-if cmp -s test_scrypt.log test_scrypt.good; then
+test_scrypt_binary=$1
+test_scrypt_good=$2
+
+if [ -z $test_scrypt_binary ] || [ -z $test_scrypt_good ]; then
+	echo -n "Test binary or good file not given: "
+	echo "Using default values for in-source-tree build."
+	test_scrypt_binary="./test_scrypt"
+	test_scrypt_good="./test_scrypt.good"
+fi
+
+$test_scrypt_binary > test_scrypt.log
+if cmp -s test_scrypt.log $test_scrypt_good; then
 	rm test_scrypt.log
 	echo "PASSED!"
 else
