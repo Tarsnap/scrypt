24ad7759bedbe0f9555edcee7f759e86adf84d32
Graham Percival <gperciva@tarsnap.com>
2016-04-20 10:33:31 -0700

    tests: cosmetic changes
    Adds $basename and comments.

--- test/test_scrypt.sh
+++ test/test_scrypt.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# File locations (allowing flexible out-of-tree builds)
+# File locations (allowing flexible out-of-tree builds).
 scrypt_binary=$1
 test_scrypt_binary=$2
 reference_txt=$3
@@ -13,7 +13,9 @@ encrypted_file="attempt.enc"
 decrypted_file="attempt.txt"
 decrypted_reference_file="attempt_reference.txt"
 
-# Check for parameters
+################################ Setup variables from the command-line
+
+# Check for required arguments.
 if [ -z $scrypt_binary ] || [ -z $test_scrypt_binary ] || \
     [ -z $reference_txt ] || [ -z $reference_enc ]; then
 	printf "Error: Scrypt binary, test binary, good file, or good "
@@ -30,9 +32,13 @@ if [ ! -f $scrypt_binary ] || [ ! -f $test_scrypt_binary ] || \
 	exit 1
 fi
 
-# Test functions
+################################ Test functions
+
 test_known_values() {
-	printf "Test 01: Generate known test values... "
+	basename="01-generate-known-test-values"
+	printf "Running test: $basename... "
+
+	# Run actual test command.
 	$test_scrypt_binary > $known_values
 
 	# The generated values should match the known good values.
@@ -48,7 +54,10 @@ test_known_values() {
 }
 
 test_encrypt_file() {
-	printf "Test 02: Encrypt a file... "
+	basename="02-encrypt-a-file"
+	printf "Running test: $basename... "
+
+	# Run actual test command.
 	echo $password | $scrypt_binary enc -P $reference_txt \
 		$encrypted_file
 
@@ -68,7 +77,10 @@ test_encrypt_file() {
 }
 
 test_decrypt_file() {
-	printf "Test 03: Decrypt a file... "
+	basename="03-decrypt-a-file"
+	printf "Running test: $basename... "
+
+	# Run actual test command.
 	echo $password | $scrypt_binary dec -P $encrypted_file \
 		$decrypted_file
 
@@ -86,7 +98,10 @@ test_decrypt_file() {
 }
 
 test_decrypt_reference_file() {
-	printf "Test 04: Decrypt a reference encrypted file... "
+	basename="04-decrypt-a-reference-encrypted-file"
+	printf "Running test: $basename... "
+
+	# Run actual test command.
 	echo $password | $scrypt_binary dec -P $reference_enc \
 		$decrypted_reference_file
 
@@ -102,12 +117,14 @@ test_decrypt_reference_file() {
 	return "$retval"
 }
 
-# Run tests
+################################ Run tests
+
+# Run tests.
 test_known_values &&			\
 	test_encrypt_file &&		\
 	test_decrypt_file &&		\
 	test_decrypt_reference_file	\
 
-# Return value to Makefile
+# Return value to Makefile.
 exit $?
 
