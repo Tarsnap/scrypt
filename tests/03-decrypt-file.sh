6aeada486ffe7c2808381abfa6f17db310847a40
Graham Percival <gperciva@tarsnap.com>
2016-10-25 10:26:55 -0700

    tests: allow multiple exit codes
    The previous test suite could only handle a single command in each
    ??-*.sh scenario file.  This change allows multiple commands to be
    tested in each scenario by writing all return values (and valgrind
    log files) to separate files.
    
    This change now relies on the setup_check_variables() function and
    some "global" variables (prefixed by $s_ or $c_) in order to
    minimize ugly boilerplate in the ??-*.sh scenario files.
    
    Note that the awkward
    	(
    		cmd...
    		echo $? > $c_exitfile
    	)
    construct is used because ${cmd} might not be exiting before we
    want the test suite to advance to the next command.  This likely
    won't apply to test scenarios in scrypt, but when we apply this
    code to spipe, being able to grab the return code of a server
    which began 10 shell commands ago (by adding & after the final
    parenthesis) is absolutely necessary.  I thought the test suite
    (as a whole) would be easier to understand if I used that
    construct everywhere, instead of only for certain scenarios.

--- tests/03-decrypt-file.sh
+++ tests/03-decrypt-file.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
 ### Constants
-scenario_valgrind_min=1
-
-decrypted_file="attempt.txt"
+c_valgrind_min=1
+encrypted_file="${out}/attempt.enc"
+decrypted_file="${out}/attempt.txt"
+reference_file="${scriptdir}/test_scrypt.good"
 
 # This requires the file created in 02-encrypt-file.sh
 scenario_cmd() {
-	echo $password | $val_cmd $bindir/scrypt dec -P \
-		$out/$encrypted_file $out/$decrypted_file
-	cmd_retval=$?
-
-	echo "$cmd_retval"
-}
-
-scenario_check() {
-	retval=$1
+	# Decrypt a file.
+	setup_check_variables
+	(
+		echo ${password} | ${c_valgrind_cmd} ${bindir}/scrypt	\
+		    dec -P ${encrypted_file} ${decrypted_file}
+		echo $? > ${c_exitfile}
+	)
 
 	# The decrypted file should match the reference.
-	if ! cmp -s $out/$decrypted_file $scriptdir/test_scrypt.good; then
-		retval=1
-	fi
-
-	echo "$retval"
+	setup_check_variables
+	if cmp -s ${decrypted_file} ${reference_file}; then
+		echo "0"
+	else
+		echo "1"
+	fi > ${c_exitfile}
 }
