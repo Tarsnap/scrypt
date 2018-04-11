ea1132326a3500822fc1f29be5ce7df7ea81ad2f
Graham Percival <gperciva@tarsnap.com>
2018-04-11 13:23:07 -0700

    tests: add "decrypt with bad passphrase"

--- tests/02-decrypt-reference-file.sh
+++ tests/02-decrypt-reference-file.sh
@@ -5,6 +5,8 @@ c_valgrind_min=1
 reference_file="${scriptdir}/test_scrypt.good"
 encrypted_reference_file="${scriptdir}/test_scrypt_good.enc"
 decrypted_reference_file="${out}/attempt_reference.txt"
+decrypted_badpass_file="${out}/decrypt-badpass.txt"
+decrypted_badpass_log="${out}/decrypt-badpass.log"
 
 scenario_cmd() {
 	# Decrypt a reference file.
@@ -19,6 +21,26 @@ scenario_cmd() {
 	# The decrypted reference file should match the reference.
 	setup_check_variables
 	if cmp -s ${decrypted_reference_file} ${reference_file}; then
+		echo "0"
+	else
+		echo "1"
+	fi > ${c_exitfile}
+
+	# Attempt to decrypt it with an incorrect passphrase.  We want this
+	# command to fail, so we negate the normal return code.
+	setup_check_variables
+	(
+		! echo "bad-pass" | ${c_valgrind_cmd} ${bindir}/scrypt	\
+		    dec -P ${encrypted_reference_file}			\
+		    ${decrypted_badpass_file}				\
+		    2> ${decrypted_badpass_log}
+		echo $? > ${c_exitfile}
+	)
+
+	# We should have received an error message.
+	setup_check_variables
+	if grep -q "scrypt: Passphrase is incorrect" \
+	    ${decrypted_badpass_log}; then
 		echo "0"
 	else
 		echo "1"
