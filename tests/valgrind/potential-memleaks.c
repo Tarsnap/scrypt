ce8529c2d09bd2e5ae2d41dba5274df6017e2eaa
Graham Percival <gperciva@tarsnap.com>
2020-12-03 22:30:27 -0700

    valgrind suppressions: FreeBSD strerror()

--- tests/valgrind/potential-memleaks.c
+++ tests/valgrind/potential-memleaks.c
@@ -30,6 +30,15 @@ pl_freebsd_printf_space_newline(void)
 	printf(" \n");
 }
 
+/* Problem with FreeBSD 12.0 and strerror(). */
+static void
+pl_freebsd_strerror(void)
+{
+	char * str = strerror(0);
+
+	(void)str; /* UNUSED */
+}
+
 #define MEMLEAKTEST(x) { #x, x }
 static const struct memleaktest {
 	const char * const name;
@@ -37,7 +46,8 @@ static const struct memleaktest {
 } tests[] = {
 	MEMLEAKTEST(pl_freebsd_fgets),
 	MEMLEAKTEST(pl_freebsd_printf_space),
-	MEMLEAKTEST(pl_freebsd_printf_space_newline)
+	MEMLEAKTEST(pl_freebsd_printf_space_newline),
+	MEMLEAKTEST(pl_freebsd_strerror)
 };
 static const int num_tests = sizeof(tests) / sizeof(tests[0]);
 
