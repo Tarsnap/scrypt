Tarsnap/libcperciva

valgrind suppressions: printf() on FreeBSD
This is necessary for the new valgrind-devel 3.17.0.g20200723,1 package.

tests/valgrind/potential-memleaks.c
@@ -13,11 +13,18 @@ pl_freebsd_link_lrt(void)
 
 /* Problem with FreeBSD 11.0 and printf(). */
 static void
-pl_freebsd_printf(void)
+pl_freebsd_printf_space(void)
 {
-	const char * printme = "";
 
-	printf("%s", printme);
+	printf(" ");
+}
+
+/* Problem with FreeBSD 12.0 and printf(). */
+static void
+pl_freebsd_printf_space_newline(void)
+{
+
+	printf(" \n");
 }
 
 /* Problem with FreeBSD 11.0 and getdelim(). */
@@ -73,7 +80,8 @@ static const struct memleaktest {
 	void (* const volatile func)(void);
 } tests[] = {
 	MEMLEAKTEST(pl_freebsd_link_lrt),
-	MEMLEAKTEST(pl_freebsd_printf),
+	MEMLEAKTEST(pl_freebsd_printf_space),
+	MEMLEAKTEST(pl_freebsd_printf_space_newline),
 	MEMLEAKTEST(pl_freebsd_getdelim),
 	MEMLEAKTEST(pl_freebsd_strlen),
 	MEMLEAKTEST(pl_nss_getgrnam),

Tarsnap/scrypt

tests/valgrind/potential-memleaks.c
@@ -14,12 +14,30 @@ pl_freebsd_fgets(void)
 		exit(1);
 }
 
+/* Problem with FreeBSD 12.0 and printf(). */
+static void
+pl_freebsd_printf_space(void)
+{
+
+	printf(" ");
+}
+
+/* Problem with FreeBSD 12.0 and printf(). */
+static void
+pl_freebsd_printf_space_newline(void)
+{
+
+	printf(" \n");
+}
+
 #define MEMLEAKTEST(x) { #x, x }
 static const struct memleaktest {
 	const char * const name;
 	void (* const volatile func)(void);
 } tests[] = {
-	MEMLEAKTEST(pl_freebsd_fgets)
+	MEMLEAKTEST(pl_freebsd_fgets),
+	MEMLEAKTEST(pl_freebsd_printf_space),
+	MEMLEAKTEST(pl_freebsd_printf_space_newline)
 };
 static const int num_tests = sizeof(tests) / sizeof(tests[0]);
 