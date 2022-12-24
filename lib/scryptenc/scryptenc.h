a2a61ffe91c6b10c586624b8c51ebba6bebfe58d
Graham Percival <gperciva@tarsnap.com>
2022-12-24 10:36:38 -0800

    scryptenc: add _print_error() to handle error strings

--- lib/scryptenc/scryptenc.h
+++ lib/scryptenc/scryptenc.h
@@ -94,6 +94,14 @@ struct scryptenc_params {
 struct scryptdec_file_cookie;
 
 /**
+ * scryptenc_print_error(rc, infilename, outfilename):
+ * Print the error corresponding to ${rc}.  If relevant, use ${infilename}
+ * or ${outfilename} to display an error about reading or writing; these
+ * arguments can be NULL to indicate standard input or output.
+ */
+void scryptenc_print_error(int, const char *, const char *);
+
+/**
  * scryptenc_buf(inbuf, inbuflen, outbuf, passwd, passwdlen,
  *     params, verbose, force):
  * Encrypt ${inbuflen} bytes from ${inbuf}, writing the resulting
