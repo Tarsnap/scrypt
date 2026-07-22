886fc3db902a4dcb76ec746899faec9e140aff20
Graham Percival <gperciva@tarsnap.com>
2026-07-22 12:00:51 -0700

    tests: add check-params-reference
    This uses the same source file as check-params-normal [*], but
    links it only to "reference" algorithm implementations, not the
    cpu-intrinsics-aware implementations.
    
    [*] tests/check-params/check-params.c, which was added in the
        previous commit.

--- tests/10-check-params.sh
+++ tests/10-check-params.sh
@@ -9,4 +9,9 @@ scenario_cmd() {
 	setup_check "check-params-normal"
 	${c_valgrind_cmd} "${bindir}/tests/check-params/check-params-normal"
 	echo $? > "${c_exitfile}"
+
+	# Check parameters with the reference function.
+	setup_check "check-params-reference"
+	${c_valgrind_cmd} "${bindir}/tests/check-params/check-params-reference"
+	echo $? > "${c_exitfile}"
 }
